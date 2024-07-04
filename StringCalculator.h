57b77a4291a4eab9ef1e7eaa66348c27bf073856
JyoshnaKodavali <73461390+JyoshnaKodavali@users.noreply.github.com>
2024-07-04 16:19:58 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,3 +1,11 @@
 int add(const char* input){
   return 0;
 }
+
+int add(const char* input) {
+    // Convert input string to integer
+    int num = atoi(input);
+    
+    // Return the number (for this test case, it's just returning the parsed integer)
+    return num;
+}
