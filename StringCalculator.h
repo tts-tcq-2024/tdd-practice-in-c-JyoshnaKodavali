d23061a269a4e721249679749a4ffea25a6c1f0a
JyoshnaKodavali <73461390+JyoshnaKodavali@users.noreply.github.com>
2024-08-01 11:51:15 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,62 +1,98 @@
-int sum(char *input)
-{
-   char *token;
-    int sum = 0;
+#include <string.h>
+#include<stdlib.h>
 
-   char delimiters[15]="$#\n:,;//\&#%";
-  
-    token = strtok(input, delimiters);
-   
-    while (token != NULL) {
-        // Convert token to integer using atoi
-        int num = atoi(token);
-        if (num <= 1000) {
-        // Add to the sum
-        sum += num;
-        }
-        // Get next token
-        token = strtok(NULL, ",");
+char error_message[128]= "";
+
+int isemptystring(const char* input)
+{
+    if (input == NULL || input[0] == '\0')
+    {
+        return 1;
     }
-
-   return sum; 
-}
-int isempty(const char *input)
-{
-   if (strlen(input)==0)
-   {
-        return 1; 
-   }
-        return 0; 
+    return 0;
 }
 
-//int issinglechar(const char *input)
-int GetSum(const char *input)
-{
-   int ret;
-   char input1[15];
-   strcpy(input1,input);
-   if(strlen(input1)==1)
-   {
-      ret = atoi(input1);
-   }
-   else
-   {
-      ret = sum(input1);  // ProvideSum only if it is not a single char
-   }
-return ret;
+int islessthanthousand(const char *input_seg)
+{    
+    int sum = 0;
+    int value_check = atoi(input_seg);
+    if(value_check < 1000)
+    {
+        sum += value_check;
+        return sum;
+    }
+    return 0;
+     
 }
 
-int add(const char *input) 
+int Checkifnegative(const char* input, char* delimiter)
 {
-   int ret=1;
-   if(isempty(input))
-   {
-      ret = 0;
-   }
-   else
-   {
-     ret = GetSum(input);
-   }
-  
-   return ret;
+    char* dup_input = NULL;
+    dup_input = strdup (input);
+    char* input_seg = strtok(dup_input,delimiter);
+    while(input_seg != NULL)
+    {
+        int value_check = atoi(input_seg);
+        if(value_check < 0)
+        {
+            return -1;
+        }
+        input_seg = strtok(NULL,delimiter);
+    }
+    return 0;
+}
+
+void appendcustomdelimiter(const char* input, char* delimiter)
+ {
+    int i = 2; 
+    while(input[i] != '\n')
+    {
+        char temp[1] = {input[i]};
+        strncat (delimiter, temp,1);
+        i = i+1;
+    }
+}
+
+void checkcustomdelimiter(const char* input, char* delimiter)
+{
+    if (input[0] == '/' && input[1] == '/')
+    {
+        strcpy(delimiter, "");
+        appendcustomdelimiter(input,delimiter);
+    }
+}
+
+int calculatesum(const char* input, char* delimiter)
+{
+    int sum = 0;
+    char* dup_input = NULL;
+    dup_input = strdup (input);
+    char* input_seg = strtok(dup_input,delimiter);
+    while(input_seg != NULL)
+    {
+        sum = sum + islessthanthousand(input_seg);
+        input_seg = strtok(NULL,delimiter);
+    }
+    return sum;
+}
+
+int add (const char* input)
+{
+    char delimiter[128] = ",\n";
+    if (1 == isemptystring(input))
+    {
+        return 0;
+    }
+    checkcustomdelimiter(input, delimiter);
+    if(Checkifnegative(input, delimiter) == -1)
+    {
+        strcpy(error_message, "Negative not allowed");
+        return -1;
+    }
+    else
+    {
+        return calculatesum(input, delimiter);
+    }
+    
+    
 }
